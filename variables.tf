425d121fee20e6fe5967fd49b750957a8375dba1
dikson <Dimitri.Korgalidze@syngenta.com>
2024-04-12 09:38:48 +0400

    format changes

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "owner" {
-  type  = string
-  Owner = "VRTX"
+  type    = string
+  default = "VRTX"
 }
 
 variable "env" {
@@ -9,12 +9,14 @@ variable "env" {
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  description = "The CIDR block of the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  type = list(any)
+  description = "The CIDR blocks of the public subnets"
+  type        = list(any)
   default = [
     "10.0.1.0/24",
     "10.0.2.0/24",
@@ -23,7 +25,8 @@ variable "public_subnet_cidrs" {
 }
 
 variable "private_subnet_cidrs" {
-  type = list(any)
+  description = "The CIDR blocks of the private subnets"
+  type        = list(any)
   default = [
     "10.0.11.0/24",
     "10.0.22.0/24",
